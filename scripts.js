bc11a121b6888529958205ed893c71a4577c8bf2
worius <nahuelcruz96@gmail.com>
2021-06-01 13:04:24 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ var score = 0;
 var vidas = 4;
 
 function uno(){
-    var Respuesta1 = parseInt(prompt('%cPregunta n° 1: De que color es la franja media de la bandera argentina?\n1. Verde \n2. Azul \n3.Blanca \n4. Roja', "Ingrese el numero de su Respuesta"));
+    var Respuesta1 = parseInt(prompt('Pregunta n° 1: De que color es la franja media de la bandera argentina?\n1. Verde \n2. Azul \n3.Blanca \n4. Roja', "Ingrese el numero de su Respuesta"));
     if(Respuesta1 == 3){
         console.log('%cCORRECTO!!!', 'color: green;');
         score++;
@@ -24,9 +24,7 @@ function uno(){
     }
 }
 function dos(){
-    console.log('%cPregunta n° 2: Cual es el ingrediente que le da el color caracteristico al locro?', 'color: yellow;');
-    console.log("1. Apio \n2. Zanahoria \n3.Cerdo \n4. Calabaza");
-    var Respuesta2 = parseInt(prompt("Ingrese el numero de su Respuesta"));
+    var Respuesta2 = parseInt(prompt('Pregunta n° 2: Cual es el ingrediente que le da el color caracteristico al locro?\n1. Apio \n2. Zanahoria \n3.Cerdo \n4. Calabaza', "Ingrese el numero de su Respuesta"));
     if(Respuesta2 == 4){
         console.log('%cCORRECTO!!!', 'color: green;');
         score++;
@@ -46,9 +44,7 @@ function dos(){
     }
 }
 function tres(){
-    console.log('%cPregunta n° 3: Que se celebra el dia 20 de junio en Argentina?', 'color: yellow;');
-    console.log("1. Dia de la Bandera Nacional \n2. Dia de la Independecia \n3. Dia del Veterano de Malvinas \n4. Dia del Trabajador");
-    var Respuesta3 = parseInt(prompt("Ingrese el numero de su Respuesta"));
+    var Respuesta3 = parseInt(prompt('Pregunta n° 3: Que se celebra el dia 20 de junio en Argentina?\n1. Dia de la Bandera Nacional \n2. Dia de la Independecia \n3. Dia del Veterano de Malvinas \n4. Dia del Trabajador', "Ingrese el numero de su Respuesta"));
     if(Respuesta3 == 1){
         console.log('%cCORRECTO!!!', 'color: green;');
         score++;
@@ -68,9 +64,7 @@ function tres(){
     }
 }
 function cuatro(){
-    console.log('%cPregunta n° 4: Cuanto hubiesemos tardado en llegar a cualquier parte del mundo desde la estratosfera?', 'color: yellow;');
-    console.log("1. 30 min \n2. 1 hora \n3. 1 Semana \n4. 5 horas");
-    var Respuesta4 = parseInt(prompt("Ingrese el numero de su Respuesta"));
+    var Respuesta4 = parseInt(prompt('Pregunta n° 4: Cuanto hubiesemos tardado en llegar a cualquier parte del mundo desde la estratosfera?\n1. 30 min \n2. 1 hora \n3. 1 Semana \n4. 5 horas', "Ingrese el numero de su Respuesta"));
     if(Respuesta4 == 2){
         console.log('%cCORRECTO!!!', 'color: green;');
         score++;
@@ -90,9 +84,7 @@ function cuatro(){
     }
 }
 function cinco(){
-    console.log('%cPregunta n° 5: Quien es el Mejor luchador de 100% lucha?', 'color: yellow;');
-    console.log("1. Viloni \n2. Rulo Verde \n3. Mosquito \n4. La Masa");
-    var Respuesta5 = parseInt(prompt("Ingrese el numero de su Respuesta"));
+    var Respuesta5 = parseInt(prompt('Pregunta n° 5: Quien es el Mejor luchador de 100% lucha?\n1. Viloni \n2. Rulo Verde \n3. Mosquito \n4. La Masa', "Ingrese el numero de su Respuesta"));
     if(Respuesta5 == 4){
         console.log('%cCORRECTO!!!', 'color: green;');
         score++;
